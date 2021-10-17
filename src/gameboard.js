1145200577247f5bdb42ad2aff453172ff4544bf
ngvuong <vminguyen@ucdavis.edu>
2021-10-17 00:31:08 -0700

    Add board with coordinates and test

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,4 +1,15 @@
 export function Gameboard() {
+  const board = {};
+
+  const xCoord = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+  const yCoord = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const gridCoords = xCoord.map((x) => {
+    return yCoord.map((y) => x + y);
+  });
+  gridCoords.forEach((row) =>
+    row.forEach((position) => (board[position] = null))
+  );
+
   const placeShip = (ship, coordinates) => {
     ship.setPositions(coordinates);
     return ship.getPositions();
@@ -6,5 +17,5 @@ export function Gameboard() {
 
   const receiveAttack = (coordinates) => {};
 
-  return { placeShip };
+  return { board, placeShip };
 }
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -6,7 +6,16 @@ beforeAll(() => {
   ship = Ship(2);
 });
 
-it("creates the gameboard", () => {
+it("creates a gameboard with 100 named coordinates", () => {
+  // expect(Gameboard().board["A1"]).toBe(null);
+  // expect(Gameboard().board["B1"]).toBe(null);
+  const board = Gameboard().board;
+  Object.keys(board).forEach((key) => {
+    expect(board[key]).toBe(null);
+  });
+});
+
+it("places the ship on the board", () => {
   // const ship = Ship(2);
   expect(Gameboard().placeShip(ship, ["a1", "a2"])).toEqual({
     a1: { isHit: false },
