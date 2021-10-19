520c925dc7a2310b863d6256f37b685108443988
ngvuong <vminguyen@ucdavis.edu>
2021-10-18 23:39:57 -0700

    Add simple bot and random attack test

--- src/player.js
+++ src/player.js
@@ -1,7 +1,36 @@
 export function Player() {
+  const coordsAttacked = [];
+
   const attack = (enemyBoard, coordinates) => {
-    return enemyBoard.receiveAttack(coordinates);
+    if (!coordsAttacked.includes(coordinates)) {
+      const outcome = enemyBoard.receiveAttack(coordinates);
+      coordsAttacked.push(coordinates);
+      return outcome;
+    }
   };
 
   return { attack };
+}
+
+export function Ai() {
+  const coordsAttacked = [];
+  const ai = Player();
+  const x = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+  const y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  const attackingAi = {
+    randomAttack: (enemyBoard) => {
+      const xIndex = Math.floor(Math.random() * 10);
+      const yIndex = Math.floor(Math.random() * 10);
+      const coordinates = x[xIndex] + y[yIndex];
+      if (!coordsAttacked.includes(coordinates)) {
+        ai.attack(enemyBoard, coordinates);
+        coordsAttacked.push(coordinates);
+        console.log(coordinates);
+        return coordinates;
+      } else randomAttack(enemyBoard);
+    },
+  };
+
+  return Object.assign(ai, attackingAi);
 }
--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,4 +1,4 @@
-import { Player } from "../player";
+import { Player, Ai } from "../player";
 import { Gameboard } from "../gameboard";
 import { Ship } from "../ship";
 
@@ -6,13 +6,30 @@ const player = Player();
 const playerBoard = Gameboard();
 const playerShip = Ship(2);
 playerBoard.placeShip(playerShip, ["A1", "B1"]);
+const playerShip2 = Ship(2);
+playerBoard.placeShip(playerShip2, ["A2", "B2"]);
 
-const ai = Player();
+const ai = Ai();
 const aiBoard = Gameboard();
 const aiShip = Ship(2);
 aiBoard.placeShip(aiShip, ["C1", "D1"]);
 
 it("attacks ai board and hits a ship", () => {
   expect(player.attack(aiBoard, "C1")).toBe(-1);
-  // expect(aiBoard.board["C1"]).toBe(-1);
+});
+
+it("attacks player board and sinks a ship", () => {
+  expect(ai.attack(playerBoard, "A1")).toBe(-1);
+  expect(ai.attack(playerBoard, "B1")).toBe(-1);
+  expect(playerShip.isSunk()).toBe(true);
+});
+
+it("attacks player and sinks the last ship", () => {
+  expect(ai.attack(playerBoard, "A2")).toBe(-1);
+  expect(ai.attack(playerBoard, "B2")).toBe(-1);
+  expect(playerBoard.allShipsSunk()).toBe(true);
+});
+
+it("attacks player randomly", () => {
+  expect(ai.randomAttack(playerBoard)).toBeTruthy();
 });
